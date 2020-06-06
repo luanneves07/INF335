2c4f804b1eba4abd2614a9dfcb114498d75b8808
luanneves07 <luannevessilva@gmail.com>
2020-06-06 17:11:07 -0300

    PROJETO - Configuracoes eclipse

--- TB02/src/test/java/algorithms/datastructures/StackArrayTest.java
+++ TB02/src/test/java/algorithms/datastructures/StackArrayTest.java
@@ -1,15 +1,34 @@
 package test.java.algorithms.datastructures;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import main.java.algorithms.datastructures.StackArray;
 
 class StackArrayTest {
 
 	@Test
-	void testaConstrutorDoStackArray() {
-		fail("TESTE");
+	void testaInsercaoNaPilha() {
+		StackArray array = new StackArray();
+		assertEquals(0, array.size());
+		array.push(15);
+		assertEquals(1, array.size());
+	}
+	
+	@Test
+	void testaRemocaoNaPilha() {
+		StackArray array = new StackArray();
+		array.push(15);
+		array.pop();
+		assertEquals(true, array.isEmpty());
+	}
+	
+	@Test
+	void testaConsultaDoUltimoElementoAdicionadoNaPilha() {
+		StackArray array = new StackArray();
+		array.push(15);
+		assertEquals(15, array.peek());
 	}
 
-	
 }
